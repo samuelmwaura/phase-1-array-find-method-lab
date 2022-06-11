31518025fa90f595a52d0e9772860d5ea73b558b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-11 14:36:08 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+
+const superbowlWin = (array)=>{
+const found = array.find(element=>{// The found variable gets the array whose value has  been gotten as true. 
+    return element.result === 'W';  // Here a boolen is returned - the callback in find() always returns a boolean.
+});
+if(found){
+    return found.year
+}else{
+    return undefined;
+}  
+}
